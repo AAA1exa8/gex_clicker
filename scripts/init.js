9f1c43d07232043d6b02e36512d47420e3a5a58a
Mia Koudelková <mia@saygex.gay>
2025-04-24 23:08:50 +0200

    Add dev/showcase mode

--- scripts/init.js
+++ scripts/init.js
@@ -4,14 +4,24 @@ function initStorage(key, value) {
     }
 }
 
-initStorage("bi", 0);
-initStorage("gay", 0);
-initStorage("trans", 0);
-initStorage("poly", 0);
+function init() {
+    initStorage("bi", 0);
+    initStorage("gay", 0);
+    initStorage("trans", 0);
+    initStorage("poly", 0);
 
-initStorage("bi-per-click", 1);
-initStorage("gay-per-click", 0);
-initStorage("trans-per-click", 0);
-initStorage("poly-per-click", 0);
+    initStorage("bi-per-click", 1);
+    initStorage("gay-per-click", 0);
+    initStorage("trans-per-click", 0);
+    initStorage("poly-per-click", 0);
 
-initStorage("currency-generators", JSON.stringify([]));
+    initStorage("currency-generators", JSON.stringify([]));
+}
+
+function dev() {
+    localStorage.clear();
+    sessionStorage.setItem("dev", true);
+    location.reload();
+}
+
+init();

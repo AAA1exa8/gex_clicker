ecc15612bb466ef5467275ef142b3f4733078b85
Mia Koudelková <mia@saygex.gay>
2025-04-24 02:03:35 +0200

    Dynamically assigned heart

--- scripts/game-animations.js
+++ scripts/game-animations.js
@@ -2,9 +2,14 @@ let clickInhibit = false;
 
 let heart = document.getElementById("clicker-heart");
 
+let image = localStorage.getItem("heart_image");
+if (!image) {
+    localStorage.setItem("heart_image", image = "sibex");
+}
+heart.style.backgroundImage = `url("/public/images/${image}-static.png")`;
+
 heart.addEventListener("click", () => {
     if (heart.classList.contains("clicked") || heart.classList.contains("regen")) return;
-    const image = localStorage.getItem("heart_image");
     heart.style.backgroundImage = `url("/public/images/${image}.gif")`;
     heart.classList.add("clicked");
     setTimeout(() => {

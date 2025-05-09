a4959f11fb8d98250efa3bd334e6f657d5ea95e0
Mia Koudelková <mia@saygex.gay>
2025-05-09 13:47:26 +0200

    Fix main screen animations

--- scripts/game-animations.js
+++ scripts/game-animations.js
@@ -1,17 +1,21 @@
 let heart = document.getElementById("clicker-heart");
 
 let image = localStorage.getItem("heart-image");
-let ext = {
-    static: localStorage.getItem("heart-static-ext"),
-    anim: localStorage.getItem("heart-anim-ext")
-};
-heart.style.backgroundImage = `url("/public/images/${image}-static.${ext.static}")`;
+heart.src = `/public/images/${image}-static.webp`;
 
 const clickEvent = new Event("manualgain");
 
+
+
 heart.addEventListener("click", () => {
     if (heart.classList.contains("clicked") || heart.classList.contains("regen")) return;
-    heart.style.backgroundImage = `url("/public/images/${image}.${ext.anim}")`;
+    if (navigator.userAgent.search("Firefox") > -1) {
+        // Firefox keeps playing it in the background.
+        // No other solution worked, but at least it's only 4kiB...
+        heart.src = `/public/images/${image}.webp?v=${Date.now()}`;
+    } else {
+        heart.src = `/public/images/${image}.webp`;
+    }
     heart.classList.add("clicked");
     setTimeout(() => {
         heart.classList.remove("clicked");
@@ -19,7 +23,7 @@ heart.addEventListener("click", () => {
         heart.dispatchEvent(clickEvent);
     }, 200);
     setTimeout(() => {
-        heart.style.backgroundImage = `url("/public/images/${image}-static.${ext.static}")`;
+        heart.src = `/public/images/${image}-static.webp`;
         heart.classList.remove("regen");
-    }, 2000);
+    }, 2100);
 });

cdc2ef1f783aa4163713d16cd635827ee2d812e6
Mia Koudelková <mia@saygex.gay>
2025-05-09 13:54:37 +0200

    Inhibit click when charging

--- scripts/game-animations.js
+++ scripts/game-animations.js
@@ -8,7 +8,7 @@ const clickEvent = new Event("manualgain");
 
 
 heart.addEventListener("click", () => {
-    if (heart.classList.contains("clicked") || heart.classList.contains("regen")) return;
+    if (heart.getAnimations().length > 0) return;
     if (navigator.userAgent.search("Firefox") > -1) {
         // Firefox keeps playing it in the background.
         // No other solution worked, but at least it's only 4kiB...
